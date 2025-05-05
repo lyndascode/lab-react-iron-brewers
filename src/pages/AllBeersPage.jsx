56ead8d66114ceef000975f7afaafcd94320696d
lynda Sadoudi <lyndaironhack@gmail.com>
2025-05-05 08:54:31 +0200

    end

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "../components/Search";
 import beersJSON from "./../assets/beers.json";
-
+import axios from "axios";
 
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
-  const [beers, setBeers] = useState(beersJSON);
+  const [beers, setBeers] = useState([]);
 
 
 
@@ -16,7 +16,16 @@ function AllBeersPage() {
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
 
-
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        setBeers(response.data); //  stocke les bières dans le state
+      })
+      .catch((error) => {
+        console.log("Error importation:", error);
+      });
+  }, []);
 
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
   return (
